d83e322ebff219d21b533000270da74053270ae2
Sai Rama Doddi <sairama1026@gmail.com>
2025-11-23 18:01:49 +0530

    added llm file

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import LLM from "../src/pages/LLM.tsx";
 
-
+const path = window.location.pathname;
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+   {path === "/llm" ? <LLM /> : <App />}
   </StrictMode>,
 )
